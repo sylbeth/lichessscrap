fd068cda76ddee560ad2c85b92b7aeff2886b2a1
Sílvia Elisabeth Romero García <sylvielizshadow@gmail.com>
2025-04-06 05:34:13 +0200

    remove unnecessary text from main

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use std::{env::args, error::Error, fs::File};
 use pgn_reader::BufferedReader;
 
 use crawler::Crawler;
-use game::Game;
-use r#move::Move;
 
 mod collector;
 mod comments;
@@ -18,16 +16,16 @@ mod stats;
 fn main() -> Result<(), Box<dyn Error>> {
     if let Some(file) = args().nth(1) {
         let mut pgn = BufferedReader::new(zstd::Decoder::new(File::open(file)?)?);
-    
+
         let mut crawler = Crawler::new()?;
         pgn.read_all(&mut crawler)?;
-    
+
         crawler.collector.print_comments();
         println!();
-    
+
         crawler.collector.print_headers();
         println!();
-    
+
         print!("{}", crawler.stats);
     }
 
