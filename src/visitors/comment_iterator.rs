fc238e66078329764401be89bc45864131788533
Sílvia Elisabeth Romero García <silviae.romgar@gmail.com>
2025-05-27 18:16:30 +0200

    wrap up comment iterator

--- src/visitors/comment_iterator.rs
+++ src/visitors/comment_iterator.rs
@@ -1,14 +1,22 @@
+//! Iterator for comments on moves of a PGN file.
+
+/// Iterator for comments on moves.
 pub struct CommentIterator<'c> {
+    /// The comment to iterate over.
     comment: &'c [u8],
+    /// A memchr iterator for spaces in the comment.
     #[cfg(feature = "memchr")]
     spaces: memchr::Memchr<'c>,
+    /// A memchr iterator for brackets in the comment.
     #[cfg(feature = "memchr")]
     brackets: std::iter::Zip<memchr::Memchr<'c>, memchr::Memchr<'c>>,
+    /// A character iterator for the characters in the comment.
     #[cfg(not(feature = "memchr"))]
     characters: std::iter::Enumerate<std::slice::Iter<'c, u8>>,
 }
 
 impl<'c> CommentIterator<'c> {
+    /// Creates a new comment iterator for the given comment.
     pub fn new(comment: &'c [u8]) -> Self {
         Self {
             comment,
@@ -49,5 +57,25 @@ impl<'c> Iterator for CommentIterator<'c> {
             }
         }
         None
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::CommentIterator;
+
+    #[test]
+    /// Tests whether the comment iterator actually works or not.
+    pub fn comment_iterator_test() {
+        let mut comment_iter = CommentIterator::new(b" [%eval 0.17] [%clk 0:00:30] ");
+        assert_eq!(
+            comment_iter.next(),
+            Some((b"%eval".as_slice(), b"0.17".as_slice()))
+        );
+        assert_eq!(
+            comment_iter.next(),
+            Some((b"%clk".as_slice(), b"0:00:30".as_slice()))
+        );
+        assert_eq!(comment_iter.next(), None);
     }
 }
