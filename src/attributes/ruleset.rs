4aa091f5eb640215b494e1e935ef3730424ba303
Sílvia Elisabeth Romero García <silviae.romgar@gmail.com>
2025-05-28 20:25:47 +0200

    reimplement ruleset

--- src/attributes/ruleset.rs
+++ src/attributes/ruleset.rs
@@ -1,60 +1,111 @@
-use std::fmt::Display;
+//! The ruleset of a Lichess game. It can be part of a tournament or a simple game ruleset.
 
-use super::AttributeFormat;
+use std::{fmt::Display, str::from_utf8};
 
-pub const FORMAT: AttributeFormat = AttributeFormat::Str("{str} <game|tournament|swiss>[ {url}]");
+use crate::tattribute;
 
-#[derive(Debug, Clone)]
-pub enum RuleSet {
-    GameMode(String),
-    Tournament(String, Tournament, String),
+use super::error::AttributeParsingError;
+
+/// A Lichess game's ruleset.
+#[derive(Debug, Default, Clone)]
+pub struct RuleSet {
+    /// The name of the ruleset.
+    name: String,
+    /// The kind of the ruleset.
+    kind: RuleSetKind,
+    /// The url id that this ruleset has.
+    url: String,
 }
 
-#[derive(Debug, Default, Clone, Copy)]
-#[repr(u8)]
-pub enum Tournament {
-    #[default]
-    NonSpecified,
-    Arena,
-    Swiss,
-}
+impl RuleSet {
+    pub fn reset(&mut self) {
+        self.name.clear();
+        self.kind = RuleSetKind::Game;
+        self.url.clear();
+    }
 
-impl From<&str> for RuleSet {
-    fn from(value: &str) -> Self {
+    /// Tries to parse a `&str` as an [`RuleSet`].
+    ///
+    /// # Errors
+    /// Will return [`AttributeParsingError`] if it's not possible to parse this string slice into an [`Elo`].
+    pub fn from_str(value: &str) -> Result<Self, AttributeParsingError> {
         let mut split_iter = value.split_terminator(' ').rev();
         if let Some(last_split) = split_iter.next() {
             if last_split == "game" {
-                return Self::GameMode(split_iter.rev().collect::<Vec<&str>>().join(" "));
+                Ok(Self {
+                    name: split_iter.rev().collect::<Vec<&str>>().join(" "),
+                    kind: RuleSetKind::Game,
+                    url: String::new(),
+                })
             } else if last_split.starts_with("https") {
                 let mut peek_split = split_iter.peekable();
                 let kind = match peek_split
                     .next_if(|value| (*value == "tournament") | (*value == "swiss"))
                 {
-                    Some("tournament") => Tournament::Arena,
-                    Some("swiss") => Tournament::Swiss,
-                    _ => Tournament::NonSpecified,
+                    Some("tournament") => RuleSetKind::Arena,
+                    Some("swiss") => RuleSetKind::Swiss,
+                    _ => return Err(ERROR),
                 };
-                return Self::Tournament(
-                    peek_split.rev().collect::<Vec<&str>>().join(" "),
+                Ok(Self {
+                    name: peek_split.rev().collect::<Vec<&str>>().join(" "),
                     kind,
-                    last_split
+                    url: last_split
                         .split_terminator('/')
                         .rev()
                         .next()
                         .map(Into::into)
                         .unwrap_or_default(),
-                );
+                })
+            } else {
+                Err(ERROR)
             }
+        } else {
+            Err(ERROR)
         }
-        Self::Tournament(value.to_owned(), Tournament::NonSpecified, String::new())
+    }
+
+    /// Tries to parse a `&[u8]` as an [`RuleSet`].
+    ///
+    /// # Errors
+    /// Will return [`AttributeParsingError`] if it's not possible to parse this bytes slice into an [`RuleSet`].
+    pub fn from_ascii(value: &[u8]) -> Result<Self, AttributeParsingError> {
+        Self::from_str(from_utf8(value).map_err(|_| ERROR)?)
+    }
+}
+
+/// The kind of RuleSet.
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum RuleSetKind {
+    /// A normal game.
+    #[default]
+    Game,
+    /// An arena tournament.
+    Arena,
+    /// A swiss tournament.
+    Swiss,
+}
+
+impl RuleSetKind {
+    /// Retrieves the representation of this [`RuleSetKind`] as a `&'static str`.
+    pub const fn as_str(&self) -> &'static str {
+        match self {
+            Self::Game => "Game",
+            Self::Arena => "Arena Tournament",
+            Self::Swiss => "Swiss Tournament",
+        }
     }
 }
 
 impl Display for RuleSet {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            RuleSet::GameMode(name) => name.fmt(f),
-            RuleSet::Tournament(name, kind, url_id) => write!(f, "{name}|{}|{url_id}", *kind as u8),
+        write!(f, "{}: {}", self.kind.as_str(), self.name)?;
+        if self.url != "" {
+            write!(f, " {}", self.url)
+        } else {
+            Ok(())
         }
     }
 }
+
+tattribute!(RuleSet, "{str} <game|tournament|swiss>[ {url}]");
