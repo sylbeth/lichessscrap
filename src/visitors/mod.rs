e0c089cdf4ae7025faa485cf9d77220357c8922c
Sílvia Elisabeth Romero García <silviae.romgar@gmail.com>
2025-05-29 18:56:42 +0200

    fix not commited visitor error logging

--- src/visitors/mod.rs
+++ src/visitors/mod.rs
@@ -1,9 +1,33 @@
-//! Module containing all the different PGN reader visitors that this application uses.
+//! Module containing all the different PGN reader visitors that this application uses and the error macros they use.
 
 pub mod checkcollect;
 pub mod comment_iterator;
-pub mod crawler;
-#[cfg(any(feature = "data", feature = "relations"))]
 pub mod serializer;
-#[cfg(feature = "stats")]
 pub mod stats;
+
+/// An error without more context than the game it happened at.
+#[macro_export]
+macro_rules! loneerror {
+    ($str:literal, $self:ident) => {
+        log::error!(concat!("{} - ", $str), $self.games);
+        $self.has_errors = true;
+    };
+}
+
+/// An error about a header of a game that is null.
+#[macro_export]
+macro_rules! nullerror {
+    ($str:literal, $self:ident) => {
+        log::error!(concat!("{} - ", $str, " is null"), $self.games);
+        $self.has_errors = true;
+    };
+}
+
+/// An error with context and the game it happened at.
+#[macro_export]
+macro_rules! valuederror {
+    ($self:ident, $error:ident) => {
+        log::error!("{} - {}", $self.games, $error);
+        $self.has_errors = true;
+    };
+}
